605f79e414aecf7d8e3fd4bf813eb5f54da259c9
Utku Boduroglu <utkubod@gmail.com>
2021-08-08 18:17:44 +0300

    feat: base code for pytorch implementation

---
+++ predict.py
@@ -0,0 +1,77 @@
+import sys
+# this is required to load the submodule library
+sys.path.insert(1, './submodules/pytorch-YOLOv4/')
+
+# Pycharm warns for this but it executes
+from tool.utils import *
+from tool.torch_utils import *
+from tool.darknet2pytorch import Darknet
+import argparse
+
+"""hyper parameters"""
+# disable CUDA for normal computers,
+# REMEMBER TO ENABLE THIS FOR THE CAR
+use_cuda = False
+
+def detect_cv2_camera(cfgfile, weightfile):
+    import cv2
+    m = Darknet(cfgfile)
+
+    m.print_network()
+    m.load_weights(weightfile)
+    print('Loading weights from %s... Done!' % (weightfile))
+
+    if use_cuda:
+        m.cuda()
+
+    cap = cv2.VideoCapture(-1)
+    # cap = cv2.VideoCapture("./test.mp4")
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    print("Starting the YOLO loop...")
+
+    num_classes = m.num_classes
+    if num_classes == 20:
+        namesfile = 'data/voc.names'
+    elif num_classes == 80:
+        namesfile = 'data/coco.names'
+    else:
+        namesfile = 'data/x.names'
+    class_names = load_class_names(namesfile)
+
+    while True:
+        ret, img = cap.read()
+        sized = cv2.resize(img, (m.width, m.height))
+        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+        start = time.time()
+        boxes = do_detect(m, sized, 0.4, 0.6, use_cuda)
+        finish = time.time()
+        print('Predicted in %f seconds.' % (finish - start))
+
+        result_img = plot_boxes_cv2(img, boxes[0], savename=None, class_names=class_names)
+
+        cv2.imshow('Yolo demo', result_img)
+        cv2.waitKey(1)
+
+    cap.release()
+
+def get_args():
+    parser = argparse.ArgumentParser('Test your image or video by trained model.')
+    parser.add_argument('-cfgfile', type=str, default='./cfg/yolov4.cfg',
+                        help='path of cfg file', dest='cfgfile')
+    parser.add_argument('-weightfile', type=str,
+                        default='./checkpoints/Yolov4_epoch1.pth',
+                        help='path of trained model.', dest='weightfile')
+    # the default value here kinda breaks things
+    parser.add_argument('-imgfile', type=str,
+                        default='./data/mscoco2017/train2017/190109_180343_00154162.jpg',
+                        help='path of your image file.', dest='imgfile')
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    detect_cv2_camera(args.cfgfile, args.weightfile)
