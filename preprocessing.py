aefbf06a5410ec315452084d98b124104e6abaa1
Eren <63977091+erentkll@users.noreply.github.com>
2021-12-26 01:17:19 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,30 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread(r'C:\Users\ereni\Desktop\deneme4.png')
+src = cv.imread(r'C:\Users\ereni\Desktop\deneme.png')
 
-img = cv.resize(img, (300, 300), interpolation = cv.INTER_AREA)
+s = 400
+
+def resize2Square(img, size, interpolation):
+  h, w = img.shape[:2]
+  c = None if len(img.shape) < 3 else img.shape[2]
+  if h == w: return cv.resize(img, (size, size), interpolation)
+  if h > w: dif = h
+  else:     dif = w
+  x_pos = int((dif - w)/2.)
+  y_pos = int((dif - h)/2.)
+  if c is None:
+    mask = np.zeros((dif, dif), dtype=img.dtype)
+    mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+  else:
+    mask = np.zeros((dif, dif, c), dtype=img.dtype)
+    mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+  return cv.resize(mask, (size, size), interpolation)
+
+resized = resize2Square(src, s, cv.INTER_AREA)
 
 #denoising
-den = cv.fastNlMeansDenoisingColored(img, None, 5, 5, 7, 21)
+den = cv.fastNlMeansDenoisingColored(resized, None, 3, 3, 7, 21)
 
 #standardization
 imgg = den.astype('float32')
@@ -15,8 +33,6 @@ std = (imgg - imgg.mean(axis=(0, 1, 2), keepdims=True)) / imgg.std(axis=(0, 1, 2
 #normalization
 norm = (std - np.min(std)) / (np.max(std) - np.min(std))
 
-cv.imshow('source', img)
-cv.imshow('denoised', den)
-cv.imshow('standardization', std)
-cv.imshow('normalization', norm)
+cv.imshow('source', src)
+cv.imshow('final', norm)
 cv.waitKey(0)
