3bd69fb79e78ef595eaf6e6ba95efc465ab84298
Utku Boduroglu <utkubod@gmail.com>
2022-02-25 16:06:26 +0300

    fix: angle clamping

--- blender3d_datagen/blender3d_generator.py
+++ blender3d_datagen/blender3d_generator.py
@@ -3,7 +3,7 @@
 # TODO: instead of passing parameters to these methods, just pass config object
 
 import bpy
-import mathutils
+import mathutils as mu
 import sys
 import numpy as np
 import json
@@ -13,14 +13,6 @@ def loadConfig(filename, mode='r'):
         data = json.load(filep)
         return data
 
-def polarToCartesian(rad, ang):
-    # x := rad * cos (ang) - rad * sin (ang)
-    # y := rad * sin (ang) + rad * cos (ang)
-    return (
-        rad*np.cos(ang) - rad*np.sin(ang),
-        rad*np.sin(ang) + rad*np.cos(ang)
-    )
-
 def createCollection(context, contextName):
     object_collection = bpy.data.collections.new(contextName)
     global_collection = context.scene.collection
@@ -28,42 +20,37 @@ def createCollection(context, contextName):
     global_collection.children.link(object_collection)
     return object_collection
 
-def spawnCamera(context):
+def spawnCameraXYZAngle(context, location, rotation):
     # we can generate new cameras with the following
     camera_data = bpy.data.cameras.new(name='Camera')
     camera_object = bpy.data.objects.new('Camera', camera_data)
 
-    camera_object.rotation_mode = 'QUATERNION'
-    camera_object.rotation_quaternion = mathutils.Quaternion((.1, .2, .1, .2))
+    camera_object.location = location
+
+    camera_object.rotation_mode = 'XYZ'
+    camera_object.rotation_euler = mu.Euler(rotation, 'XYZ')
 
     context.scene.collection.objects.link(camera_object)
 
 
-# TODO: Add parameter for specifying mesh
 def generateRandomTuples(objectCountinScene):
-    # during random generation, maybe specify the FOV of our camera(s)
-    # for occlusion and not including bounding boxes that cannot be drawn on the image
-    randGen = np.random.default_rng()
-    randomSamples = np.random.rand(objectCountinScene * 2)
-    randomPositions = randGen.choice(randomSamples, size=(objectCountinScene, 2), replace=False)
-
-    return randomPositions
+    return np.random.random_sample((objectCountinScene, 2))
 
 def randomToXYZ(points, maxRange, clampRange):
-    list(filter(lambda x: clampRange[0] <= x[1] <= clampRange[1], points))
-    cartesianPos = [polarToCartesian(p[0], p[1]*2*np.pi) for p in points]
-
-    # TODO: clamping coordinates do not work
-    objectPositions = [maxRange * (np.array([p[0], p[1], 0])) for p in cartesianPos]
-    return objectPositions
+    # lerp random tuples
+    interp_points = [ (maxRange*p[0], 2*np.pi*((clampRange[1]-clampRange[0])*p[1]-clampRange[0]))
+                      for p in points ]
+    # apply polar to cartesian transformation to tuples
+    euclidean_pts = [ (p[0]*np.cos(p[1]), p[0]*np.sin(p[1]), 0.0) for p in interp_points]
+    return euclidean_pts
 
 
 # clampRange allows us to specify angle range to insert meshes in, multiplied by 2pi
 def projectedTupleToMeshes(positions, context, clampRange=None):
     if clampRange is None:
-        clampRange = [0, 1]
+        clampRange = (0, 1)
 
-    max_radius = 10
+    max_radius = 20
 
     objectPositions = randomToXYZ(positions, max_radius, clampRange)
 
@@ -88,7 +75,7 @@ if __name__ == '__main__':
     objectCountinScene = 10
     datagenConfig = loadConfig('/home/utku/Projects/Perception/cfg/blender3d_datagen_config.json')
 
-    spawnCamera(bpy.context)
+    spawnCameraXYZAngle(bpy.context, mu.Vector((5, 5, 5)), mu.Vector((0,0,0)))
     randomPositions = generateRandomTuples(datagenConfig['3d_space_object_parameters']['max_objects_in_scene'])
     projectedTupleToMeshes(randomPositions, bpy.context,
                            datagenConfig['3d_space_object_parameters']['object_radial_interval'])
